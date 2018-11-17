4633102b19791162672b151b3e3757293cf5169c
tom.wimmenhove@gmail.com <tom.wimmenhove@gmail.com>
2018-11-17 14:48:20 -0500

    backup

--- kernel/interrupts.cpp
+++ kernel/interrupts.cpp
@@ -18,18 +18,20 @@ extern "C" void exception_double(interrupt_state* state){ panic("exception_doubl
 extern "C" void exception_invalid_tss(interrupt_state* state){ panic("exception_invalid_tss", state); }
 extern "C" void exception_seg_not_present(interrupt_state* state){ panic("exception_seg_not_present", state); }
 extern "C" void exception_stack_seg(interrupt_state* state){ panic("exception_stack_seg", state); }
-extern "C" void exception_gp(interrupt_state* state)
-{
-	con << "GP fault at rip=0x" << hex_u64(state->iregs.rip)
-		<< " with err_code=0c" << hex_u16(state->err_code) << '\n';
-	panic("exception_gp", state);
-}
 extern "C" void exception_fp(interrupt_state* state){ panic("exception_fp", state); }
 extern "C" void exception_align(interrupt_state* state){ panic("exception_align", state); }
 extern "C" void exception_mach_chk(interrupt_state* state){ panic("exception_mach_chk", state); }
 extern "C" void exception_simd_fp(interrupt_state* state){ panic("exception_simd_fp", state); }
 extern "C" void exception_virt(interrupt_state* state){ panic("exception_virt", state); }
 extern "C" void exception_sec(interrupt_state* state){ panic("exception_sec", state); }
+
+extern "C" void exception_gp(interrupt_state* state)
+{
+	con << "GP fault at rip=0x" << hex_u64(state->iregs.rip)
+		<< " with err_code=0c" << hex_u16(state->err_code) << '\n';
+	panic("exception_gp", state);
+}
+
 extern "C" void exception_page(interrupt_state* state)
 {
 	uint64_t addr = cr2_get();
@@ -37,7 +39,6 @@ extern "C" void exception_page(interrupt_state* state)
 
 	if (addr < 0x8000000000000000)
 		panic("addr < 0x8000000000000000\n", state);
-
 
 	memory::handle_pg_fault(state, addr);
 	mallocator::handle_pg_fault(state, addr);
