42363b6c6eeea9407bcbe1debc3330711146d923
tom <tom.wimmenhove@gmail.com>
2018-11-17 21:22:12 -0500

    memcpy and friends weren't extern "C"

--- kernel/klib.cpp
+++ kernel/klib.cpp
@@ -5,6 +5,12 @@
 
 extern "C" 
 {
+	void *memset(void *s, int c, size_t n);
+	void *memcpy(void *dest, const void *src, size_t n);
+	void *memmove(void *dest, const void *src, size_t n);
+	int memcmp(const void *s1, const void *s2, size_t n);
+	size_t strlen(const char *s);
+
 	int __cxa_atexit(void (*f)(void*), void *objptr, void *dso);
 	void __attribute__((noreturn)) __assert_func(const char* file, int line, const char* fn, const char* assertion);
 	void __attribute__((noreturn)) __stack_chk_fail(void);
