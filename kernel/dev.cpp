628ac15c6715161590fffd12fec7e1c54cef3b1a
tom <tom.wimmenhove@gmail.com>
2018-11-21 12:51:07 -0500

    backup

--- kernel/dev.cpp
+++ kernel/dev.cpp
@@ -7,15 +7,12 @@ std::shared_ptr<driver_handle> devices::open(int dev_type, int dev_idx)
 	con << "Trying to open " << dev_type << ':' << dev_idx << '\n';
 
 	for(auto& handle: dev_head)
-	{   
 		if (handle->dev_type == dev_type)
-		{   
 			if (handle->open(dev_idx))
 				return handle;
 			else
 				return nullptr;
-		}
-	}
+	
 	return nullptr;
 }
 
